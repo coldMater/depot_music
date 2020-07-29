99a55fe2e90b781ce2b725fb089a761774fdc7b7
coldflow <dev.coldflow@gmail.com>
2020-07-29 16:26:51 +0900

    {init}

---
+++ src/main/java/co/coldflow/depot_music/DepotMusicApplication.java
@@ -0,0 +1,13 @@
+package co.coldflow.depot_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepotMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepotMusicApplication.class, args);
+	}
+
+}
