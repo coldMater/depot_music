6bc613e2a4ef559c878b34555b9815e58978da07
coldflow <dev.coldflow@gmail.com>
2020-07-30 23:52:40 +0900

    FIRST STEP

---
+++ src/main/java/co/coldflow/depot_music/web/InstructorController.java
@@ -0,0 +1,21 @@
+package co.coldflow.depot_music.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class InstructorController {
+
+    @GetMapping("/instructors")
+    public String getInstructors() {
+        return "instructors";
+    }
+
+    @GetMapping("/instructors/new")
+    public String getInstructorForm() {
+        return "instructor/instructor_form";
+    }
+
+
+}
---
+++ src/main/java/co/coldflow/depot_music/web/dto/InstructorDto.java
@@ -0,0 +1,15 @@
+package co.coldflow.depot_music.web.dto;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class InstructorDto {
+        private String nickName;
+        private String realName;
+        private String tel;
+        private String memo;
+        private String profileInfo;
+        private String username;
+        private String password;
+        private MultipartFile portrait;
+}
