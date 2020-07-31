a38608fef740ac6b5dc4ce689074ff24d23e5055
coldMater <coldmater@gmail.com>
2020-07-31 18:02:59 +0900

    [UPDATE] 강사 정보 View -> Controller 정보 전송
    🚨파일 전송을 단순한 multipart/form-data 로 전송하고
    Command 객체(객체로 맵핑하는 방법)로 받으려 했었는데,
    
    이 경우 Command 객체에 해당 파일에 대한 필드(portrait)의
    constructor, setter 가 있는 경우에 받지 못하였고,
    getter 까지 정의되어 있어야 정상적으로 맵핑되는 것을 확인하였다.

--- src/main/java/co/coldflow/depot_music/web/InstructorController.java
+++ src/main/java/co/coldflow/depot_music/web/InstructorController.java
@@ -1,8 +1,10 @@
 package co.coldflow.depot_music.web;
 
+import co.coldflow.depot_music.web.dto.InstructorDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class InstructorController {
@@ -12,9 +14,14 @@ public class InstructorController {
         return "instructors";
     }
 
+    @PostMapping("/instructors")
+    public String postInstructor(InstructorDto instructorDto) {
+        System.out.println(instructorDto);
+        return "redirect:/instructors/new";
+    }
+
     @GetMapping("/instructors/new")
     public String getInstructorForm() {
         return "instructor/instructor_form";
     }
-
 }
--- src/main/java/co/coldflow/depot_music/web/dto/InstructorDto.java
+++ src/main/java/co/coldflow/depot_music/web/dto/InstructorDto.java
@@ -12,4 +12,92 @@ public class InstructorDto {
         private String username;
         private String password;
         private MultipartFile portrait;
+
+        public InstructorDto(String nickName, String realName, String tel, String memo, String profileInfo, String username, String password) {
+                this.nickName = nickName;
+                this.realName = realName;
+                this.tel = tel;
+                this.memo = memo;
+                this.profileInfo = profileInfo;
+                this.username = username;
+                this.password = password;
+        }
+
+        public String getNickName() {
+                return nickName;
+        }
+
+        public void setNickName(String nickName) {
+                this.nickName = nickName;
+        }
+
+        public String getRealName() {
+                return realName;
+        }
+
+        public void setRealName(String realName) {
+                this.realName = realName;
+        }
+
+        public String getTel() {
+                return tel;
+        }
+
+        public void setTel(String tel) {
+                this.tel = tel;
+        }
+
+        public String getMemo() {
+                return memo;
+        }
+
+        public void setMemo(String memo) {
+                this.memo = memo;
+        }
+
+        public String getProfileInfo() {
+                return profileInfo;
+        }
+
+        public void setProfileInfo(String profileInfo) {
+                this.profileInfo = profileInfo;
+        }
+
+        public String getUsername() {
+                return username;
+        }
+
+        public void setUsername(String username) {
+                this.username = username;
+        }
+
+        public String getPassword() {
+                return password;
+        }
+
+        public void setPassword(String password) {
+                this.password = password;
+        }
+
+        public MultipartFile getPortrait() {
+                return portrait;
+        }
+
+        public void setPortrait(MultipartFile portrait) {
+                this.portrait = portrait;
+        }
+
+        @Override
+        public String toString() {
+                return "InstructorDto{" +
+                        "nickName='" + nickName + '\'' +
+                        ", realName='" + realName + '\'' +
+                        ", tel='" + tel + '\'' +
+                        ", memo='" + memo + '\'' +
+                        ", profileInfo='" + profileInfo + '\'' +
+                        ", username='" + username + '\'' +
+                        ", password='" + password + '\'' +
+                        ", portrait='" + portrait.getOriginalFilename() + '\'' +
+                        '}';
+        }
 }
