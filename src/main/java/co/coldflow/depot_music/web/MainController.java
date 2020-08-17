c920800e4c08a3a2de6ca299616f4e99c9e04303
coldMater <coldmater@gmail.com>
2020-08-17 23:07:43 +0900

    [ADD] 네비게이션 추가 및 공통코드화(common)

---
+++ src/main/java/co/coldflow/depot_music/web/MainController.java
@@ -0,0 +1,13 @@
+package co.coldflow.depot_music.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String getInstructors(Model model) {
+        return "main/main";
+    }
+}
